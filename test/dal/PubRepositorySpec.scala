31c7994b50af8be48c0e50ef662f6a3bd580158e
Louis Houghton <louis@louishoughton.com>
2017-06-11 20:55:55 +0100

    Some PubRepository tests

---
+++ test/dal/PubRepositorySpec.scala
@@ -0,0 +1,55 @@
+package dal
+
+import org.scalatestplus.play.{PlaySpec, _}
+import play.api.test.Helpers.{GET => GET_REQUEST}
+
+import scala.concurrent.ExecutionContext
+
+class PubRepositorySpec extends PlaySpec with OneAppPerSuite {
+
+  private val pubRepository = app.injector.instanceOf[PubRepository]
+  implicit val ec = app.injector.instanceOf[ExecutionContext]
+
+  "Pub Repository" must {
+    "list pubs in order" in {
+      val eventualPubs = pubRepository.listPubs()
+      eventualPubs.map {pubs =>
+        pubs.size must not be 0
+        pubs.head.pub.name mustEqual "112 Church St"
+      }
+    }
+
+    "list pubs with visits" in {
+      val eventualPubs = pubRepository.listPubsWithVisits(1)
+      eventualPubs.map {pubs =>
+        pubs.size must not be 0
+        val firstPub = pubs.head.pubWithLocation.pub
+        firstPub.name mustEqual "112 Church St"
+        pubs.head.visit mustEqual None
+      }
+    }
+
+    "mark pub as visited" in {
+      for {
+        eventualPubs <- pubRepository.listPubsWithVisits(1)
+        _ <- pubRepository.visit(eventualPubs.head.pubWithLocation.pub.id, 1)
+        pubs <- pubRepository.listPubsWithVisits(1)
+      } yield {
+        pubs.head.visit must not be None
+      }
+    }
+
+    "mark pub as not visited" in {
+      for {
+        eventualPubs <- pubRepository.listPubsWithVisits(1)
+        _ <- pubRepository.visit(eventualPubs.head.pubWithLocation.pub.id, 1)
+        _ <- pubRepository.unVisit(eventualPubs.head.pubWithLocation.pub.id, 1)
+        pubs <- pubRepository.listPubsWithVisits(1)
+      } yield {
+        pubs.head.visit mustEqual None
+      }
+    }
+
+  }
+
+}
