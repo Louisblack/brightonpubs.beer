49b10151db0c2923ced3c73de44871677073a842
Louis Houghton <louis@louishoughton.com>
2017-06-20 09:21:05 +0100

    Revert "Offload images to S3"
    This reverts commit e4d6ad8b78eb2f1bbd2b550f77d8df49a7923e32.

--- conf/evolutions/default/15.sql
+++
@@ -1,7 +0,0 @@
--- noinspection SqlDialectInspectionForFile
-
-# --- !Ups
-
-UPDATE "pubs" SET "img_url" = replace("img_url", '/assets/images/', 'https://s3.eu-west-2.amazonaws.com/brighton-pubs-bucket/')
-
-# --- !Downs
